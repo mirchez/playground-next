2c551980025ae6dee536ecddb5bf4b5ac101aef6
Miguel <mmirandasanchez16@gmail.com>
2025-04-02 19:19:32 -0300

    updates

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,24 @@
-"use client";
+type album = {
+  id: number;
+  title: string;
+  userId: number;
+};
 
-export default function Home() {
-  throw new Error("not working");
+export default async function Home() {
+  const resp = await fetch("https://jsonplaceholder.typicode.com/albums");
+  if (!resp.ok) throw new Error("There was an error fetching");
+  const albums = await resp.json();
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Home Page
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols">
+      {albums.map((album: album) => (
+        <div
+          key={album.id}
+          className="bg-white shadow-md rounded-lg p-4 transition t..."
+        >
+          <h3 className="text-lg font-bold mb-2">{album.title}</h3>
+          <p className="text-gray-600">Album ID: {album.id}</p>
+        </div>
+      ))}
     </div>
   );
 }
---
+++ app/loading.tsx
@@ -0,0 +1,6 @@
+const loading = () => {
+  return <div>-------------------------</div>;
+};
+
+export default loading;
+//here we put what we want to show we the screen is loading
